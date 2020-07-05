3822a24eb8567269ace30a6cef26128043407ef8
root <17771340703@163.com>
2020-07-05 14:46:53 +0800

    测试一下

--- code.py
+++ code.py
@@ -1,7 +1,12 @@
 
 def code():
         tryCode = input("请输入验证码：")
-        print('您的验证码为'+tryCode)
-        return()
+        print('您的验证码为'+str(tryCode))
+        if tryCode=='123456':
+                print('验证成功')
+        else:
+                print('验证失败')
+
+
 code()
 
